4e9570cdbe28a9ce503bf9a82365c7e29fc2a90f
Justus Magin <keewis@posteo.de>
2024-10-20 23:01:48 +0200

    add a test for slices

--- xarray_array_testing/indexing.py
+++ xarray_array_testing/indexing.py
@@ -36,3 +36,17 @@ class IndexingTests(DuckArrayTestMixin):
 
         assert isinstance(actual, self.array_type), f"wrong type: {type(actual)}"
         self.assert_equal(actual, expected)
+
+    @given(st.data())
+    def test_variable_isel_slices(self, data):
+        variable = data.draw(xrst.variables(array_strategy_fn=self.array_strategy_fn))
+        idx = data.draw(indexers(variable.sizes, st.slices))
+
+        with self.expected_errors("isel_slices", variable=variable):
+            actual = variable.isel(idx).data
+
+            raw_indexers = {dim: idx.get(dim, slice(None)) for dim in variable.dims}
+            expected = variable.data[*raw_indexers.values()]
+
+        assert isinstance(actual, self.array_type), f"wrong type: {type(actual)}"
+        self.assert_equal(actual, expected)
