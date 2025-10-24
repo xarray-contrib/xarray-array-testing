0f336d3029bd11857ac1273990a4149b014abed5
Justus Magin <keewis@posteo.de>
2025-10-25 00:33:35 +0200

    parametrize by `xp`
    This requires converting the static method to an actual method.

--- xarray_array_testing/base.py
+++ xarray_array_testing/base.py
@@ -2,7 +2,6 @@ import abc
 from abc import ABC
 from types import ModuleType
 
-import numpy as np
 import numpy.testing as npt
 from xarray.namedarray._typing import duckarray
 
@@ -26,13 +25,14 @@ class DuckArrayTestMixin(ABC):
     def assert_equal(a, b):
         npt.assert_equal(a, b)
 
-    @staticmethod
-    def assert_dimension_indexers_equal(a, b):
+    def assert_dimension_indexers_equal(self, a, b):
         assert type(a) is type(b), f"types don't match: {type(a)} vs {type(b)}"
 
         if isinstance(a, dict):
             assert a.keys() == b.keys(), f"Different dimensions: {list(a)} vs {list(b)}"
 
-            assert all(np.equal(a[k], b[k]) for k in a), "Differing indexers"
+            assert all(
+                self.xp.all(self.xp.equal(a[k], b[k])) for k in a
+            ), "Differing indexers"
         else:
             npt.assert_equal(a, b)
