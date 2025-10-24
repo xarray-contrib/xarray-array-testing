dcd4ca1bb1dfb42198bf169a941f30ab4547bec9
Justus Magin <keewis@posteo.de>
2025-10-24 20:55:08 +0200

    simplify

--- xarray_array_testing/base.py
+++ xarray_array_testing/base.py
@@ -4,7 +4,6 @@ from types import ModuleType
 
 import numpy as np
 import numpy.testing as npt
-import xarray as xr
 from xarray.namedarray._typing import duckarray
 
 
@@ -34,15 +33,6 @@ class DuckArrayTestMixin(ABC):
         if isinstance(a, dict):
             assert a.keys() == b.keys(), f"Different dimensions: {list(a)} vs {list(b)}"
 
-            values = ((a[k], b[k]) for k in a)
-            assert all(
-                (
-                    isinstance(v1, xr.Variable)
-                    and isinstance(v2, xr.Variable)
-                    and v1.dims == v2.dims
-                    and np.equal(v1.data, v2.data)
-                )
-                for v1, v2 in values
-            ), "Differing indexers"
+            assert all(np.equal(a[k], b[k]) for k in a), "Differing indexers"
         else:
             npt.assert_equal(a, b)
